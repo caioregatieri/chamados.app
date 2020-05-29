a70faad9f4ea915f2d37e88a9e1e58c8e64f133e
Caio Regatieri <caio_fr_sp@hotmail.com>
2020-05-29 16:57:59 -0300

    Usar https em produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
